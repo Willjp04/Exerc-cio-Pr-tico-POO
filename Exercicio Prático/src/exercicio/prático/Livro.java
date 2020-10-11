1bc1b8a87a8c8d35cecbfa8390b9bc06393b5fb5
Willian Sostenes <sostenes655@gmail.com>
2020-10-11 14:19:51 -0300

    Criação da Classe Livro

---
+++ Exercicio Prático/src/exercicio/prático/Livro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio.prático;
+
+/**
+ *
+ * @author PORTOMASTER
+ */
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int totPaginas;
+    private int pagAtual;
+    private boolean aberto;
+    private String leitor;
+    
+    
+}
